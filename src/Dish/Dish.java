5179928777f806c2f510792e0a7a8a350f6373eb
Antibupt <2292308781@qq.com>
2020-03-23 11:59:55 +0800

    定义菜品类基本属性和方法，以及简单的说明

---
+++ src/Dish/Dish.java
@@ -0,0 +1,46 @@
+package Dish;
+
+import java.util.ArrayList;
+
+public class Dish {
+    private String Name;
+    private ArrayList<Attribute> Attrs;
+
+    Dish(String dishName){
+        //根据菜品名创建名称
+
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public ArrayList<Attribute> getAttrs() {
+        return Attrs;
+    }
+
+    public void setAttrs(ArrayList<Attribute> attrs) {
+        Attrs = attrs;
+    }
+
+    public int getAttrNum(){
+        return Attrs.size();
+    }
+
+    public boolean isOrder(){
+        //检查是否所有的Attribute都被选过一遍
+        return true;
+    }
+
+    public float calculatePrice(){
+        //计算这份菜的价格
+        return 0;
+    }
+
+
+
+}
