a503885d9cb3dd931d83a7871b9d515613d747d2
Miyane-lwd <709688109@qq.com>
2020-03-24 15:28:19 +0800

    jkhk

---
+++ src/Membership/Userinfo.java
@@ -0,0 +1,65 @@
+package Membership;
+
+
+
+    public class Userinfo {
+        private String firstname;
+        private String surname;
+        private String email;
+        private int mobileNumber;
+        private int membershipNumber;
+        private int virtualStamps;
+
+        public Userinfo(String firstname, String surname, String email, int mobileNumber, int membershipNumber, int virtualStamps) {
+            this.firstname = firstname;
+            this.surname = surname;
+            this.email = email;
+            this.mobileNumber = mobileNumber;
+            this.membershipNumber = membershipNumber;
+            this.virtualStamps = virtualStamps;
+        }
+
+        public String getFirstname() {
+            return firstname;
+        }
+        public void setFirstname(String firstname) {
+            this.firstname = firstname;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public int getMobileNumber() {
+            return mobileNumber;
+        }
+        public void setMobileNumber(int mobileNumber) {
+            this.mobileNumber = mobileNumber;
+        }
+
+        public int getMembershipNumber() {
+            return membershipNumber;
+        }
+        public void setMembershipNumber(int membershipNumber) {
+            this.membershipNumber = membershipNumber;
+        }
+
+        public int getVirtualStamps() {
+            return virtualStamps;
+        }
+        public void setVirtualStamps(int virtualStamps) {
+            this.virtualStamps = virtualStamps;
+        }
+    }
+    
+
