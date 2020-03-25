828851f4ad578b299d1d2bdfe443e3a6daacc687
Antibupt <2292308781@qq.com>
2020-03-25 19:35:35 +0800

    定义菜品类基本属性和方法，以及简单的说明

--- src/Dish/Dish.java
+++ src/Dish/Dish.java
@@ -3,16 +3,25 @@ package Dish;
 import java.util.ArrayList;
 
 public class Dish {
+    private int num;//菜品序号
     private String Name;
     private ArrayList<Attribute> Attrs;
 
-    public static ArrayList<Dish> getDishList(){
+    public static ArrayList<Dish> getDishList(){//获取所有现有菜品
         return null;
     }
 
     Dish(String dishName){
         //根据菜品名创建名称
 
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
     }
 
     public String getName() {
