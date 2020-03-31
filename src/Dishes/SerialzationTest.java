19334bf5d283dca7db55212010031c85bc841bf2
Antibupt <2292308781@qq.com>
2020-03-31 23:42:42 +0800

    All Dish methods and simple Dish loading in Iteration 1(with API Tutorial in SerialzationTest)

--- src/Dishes/SerialzationTest.java
+++ src/Dishes/SerialzationTest.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class SerialzationTest {
     public static void main(String[] args) {
@@ -56,6 +57,33 @@ public class SerialzationTest {
         System.out.println(dish2.getAttrs().get(1).isMultiSelected());
 
         dish2.getAttrs().get(1).select("Soft");
+
+
+
+
+    }
+    public static void example(){
+        //根据名称创建菜品
+        Dish dish = new Dish("HandPulledNoodle");
+        ArrayList<Dish> dishList = Dish.getDishList();
+        Dish dish2 = dishList.get(0);
+
+        //获取dish下所有菜品（attribute）
+        ArrayList<Attribute> attrs = dish2.getAttrs();
+        //获取attribute下所有选项（option）
+        ArrayList<Option> opts =attrs.get(0).getOpts();
+        //根据选项的索引选择
+        attrs.get(0).select(1);
+        //根据选项的名称选择
+        attrs.get(1).select("Medium");
+        //根据option对象选择option
+        attrs.get(0).getOpts().get(0).select();
+        //检测这个attribute是否被选择过了
+        attrs.get(0).isSelected();
+        //检测这个attribute是否发生了多选的情况
+        attrs.get(0).isMultiSelected();
+        //检测是否dish中的每个属性都被选过了
+        dish.isOrder();
 
     }
 }
