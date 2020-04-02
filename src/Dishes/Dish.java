75c2c673cfa03fe5cfac50c0393dfeb56189f226
Antibupt <2292308781@qq.com>
2020-04-02 16:41:22 +0800

    add gson jar

--- src/Dishes/Dish.java
+++ src/Dishes/Dish.java
@@ -18,7 +18,7 @@ public class Dish {
     public static ArrayList<Dish> getDishList(){
         //获取所有现有菜品列表
         List<String> childPathes = new ArrayList<String>() ;
-        File file = new File(this.getClass().getResource("/").getPath()+"\\Dishes\\Data");		//获取其file对象
+        File file = new File(Dish.class.getResource("/").getPath()+"\\Dishes\\Data");		//获取其file对象
         File[] fs = file.listFiles();	//遍历path下的文件和目录，放在File数组中
         for(File f:fs){					//遍历File[]数组
             if(!f.isDirectory())		//若非目录(即文件)，则打印
