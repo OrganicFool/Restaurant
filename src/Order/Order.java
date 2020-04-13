9471259ccd6b2dcc038fc48a79c22195a42b9d41
wyb <690521306@qq.com>
2020-04-14 02:35:34 +0800

    存订单到后台函数

--- src/Order/Order.java
+++ src/Order/Order.java
@@ -1,5 +1,10 @@
 package Order;
 
+import Dishes.Dish;
+import com.csvreader.CsvWriter;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.text.DecimalFormat;
 import java.text.Format;
 import java.text.SimpleDateFormat;
@@ -8,7 +13,7 @@ import java.util.Date;
 
 public class Order {
     private String order_date;
-    private ArrayList<Object> order_dish;
+    private String order_dish;//菜名
     private float order_price;
     private String order_pay; //支付方式
     private String order_num; //取餐号
@@ -26,10 +31,10 @@ public class Order {
         this.order_date = date;
     }
 
-    public ArrayList<Object> getOrder_dish() {
+    public String getOrder_dish() {
         return order_dish;
     }
-    public void setOrder_dish(ArrayList<Object> order_dish) {
+    public void setOrder_dish(String order_dish) {
         this.order_dish = order_dish;
     }
 
@@ -66,16 +71,85 @@ public class Order {
     public void addDish(Object object){
         //添加菜品
     }
+    public void csvheader(){
+        if(isVip){
+            try{
+                CsvWriter csvWriter = new CsvWriter("/orderData/vip.csv",',', Charset.forName("GBK"));
+                ArrayList<Object> header = new ArrayList();
+                Dish dish = new Dish(order_dish);
+                header.add("date");
+                header.add("VIP number");
+                header.add("dish name");
+                for(int i = 0; i < dish.getAttrs().size(); i++){
+                    header.add(dish.getAttrs().get(i));
+                }
+                header.add("price");
+                header.add("payment");
+                String[] array = (String[])header.toArray(new String[header.size()]);
+                csvWriter.writeRecord(array);
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            try{
+                CsvWriter csvWriter = new CsvWriter("/orderData/not_vip.csv",',', Charset.forName("GBK"));
+                ArrayList<Object> header = new ArrayList();
+                Dish dish = new Dish(order_dish);
+                header.add("date");
+                header.add("dish name");
+                for(int i = 0; i < dish.getAttrs().size(); i++){
+                    header.add(dish.getAttrs().get(i));
+                }
+                header.add("price");
+                header.add("payment");
+                String[] array = (String[])header.toArray(new String[header.size()]);
+                csvWriter.writeRecord(array);
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-    public void storeOrder(boolean isVip){
+    }
+
+    public void storeOrder(){
         if(isVip){
             //把会员订单存进会员json
+            //订单里有：时间 会员号 菜名 各种属性名以及选项 总价格
+            try{
+                CsvWriter csvWriter = new CsvWriter("/orderData/vip.csv",',', Charset.forName("GBK"));
+                ArrayList<Object> content = new ArrayList();
+                Dish dish = new Dish(order_dish);//菜名
+                content.add(order_date);//下单时间
+                content.add(vip_num);//会员号
+                content.add(order_dish);
+                for(int i = 0; i < dish.getOptions().size(); i++){ content.add(dish.getOptions().get(i)); }
+                content.add(order_price);
+                content.add(order_pay);
+                String[] array = (String[])content.toArray(new String[content.size()]);
+                csvWriter.writeRecord(array);
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
         }else{
-            //把非会员订单存进非会员json
+            //里面没有会员号
+            try{
+                CsvWriter csvWriter = new CsvWriter("/orderData/not_vip.csv",',', Charset.forName("GBK"));
+                ArrayList<Object> content = new ArrayList();
+                Dish dish = new Dish(order_dish);
+                content.add(order_date);
+                content.add(order_dish);
+                for(int i = 0; i < dish.getOptions().size(); i++){ content.add(dish.getOptions().get(i)); }
+                content.add(order_price);
+                content.add(order_pay);
+                String[] array = (String[])content.toArray(new String[content.size()]);
+                csvWriter.writeRecord(array);
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void printOrder() {
-        //读取json,生成txt
+        
     }
 }
