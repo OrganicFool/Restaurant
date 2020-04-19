c18a658ca6b66b77ad94142840ca1734e36ed7d0
Miyane-lwd <709688109@qq.com>
2020-04-19 12:17:48 +0800

    new method about check email and password strength

--- src/Membership/Meth.java
+++ src/Membership/Meth.java
@@ -73,41 +73,58 @@ public class Meth {
         } while (isNickname);
                  Userinfo user = new Userinfo();
                  user.setNickname(nickname);
+          //firstname
           System.out.println("Please input your fistname");
           user.setFirstname(scanner.next());
+          //surname
           System.out.println("Please input your surname");
           user.setSurname(scanner.next());
-          System.out.println("Please input your email");
-          user.setEmail(scanner.next());
-
-         System.out.println("请输入电话号码");
-                 user.setMobileNumber(scanner.nextInt());
+          //check email
+                  boolean isEmail=true;
+                  String email="";
+                  do{
+                      System.out.println("Please input your email");
+                      email=scanner.next();
+                      if(email.matches("\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*"))
+                      {
+                          System.out.println("right");
+                      }
+                      else
+                      {
+                          System.out.println("Err");
+                      }
+                  }while(isEmail);
+           user.setEmail(email);
+           //mobilenumber
+          System.out.println("Please input your mobile number");
+          user.setMobileNumber(scanner.nextInt());
+          //password
                  boolean isPassword = true;
                  String password = "";
-                 do {
-            System.out.println("请输入密码");
+                 do {   System.out.println("Please input your password");
                         password = scanner.next();
-
-                         if (password.length() < 6 || password.length() > 10) {
-                                 System.out.println("密码长度必须在6-10位，请重新设置\n");
+                        if (password.length() < 6 || password.length() > 10) {
+                                 System.out.println("The length of password must between 3-6，please reset\n");
                              }
-                         System.out.println("请再次输入密码");
-                        String passWord2 = scanner.next();
+                         System.out.println("please input password again");
+                         String passWord2 = scanner.next();
                         if (!password.equals(passWord2)) {
-                                 System.out.println("两次输入的密码不一致，请重新输入");
+                                 System.out.println("Two password are different，please reset");
                             } else {
-                                 isPassword = false;
+                                    isPassword = false;
                              }
-                         //System.out.println("密码强度分析：   " + strongPW(passWord));
+                         System.out.println("Password analyse：   " + strongPW(password));
                      } while (isPassword);
                  user.setPassword(password);
-                 System.out.println("密码设置成功");
-        //验证码
-                //yanZhen();没做
-                 // 密保，
+                 System.out.println("set password successfully");
+
+                 //验证码
+                 //yanZhen();没做
+                 //密保
                  //System.out.println("请随意输入一组字符，作为密保");
                  //String encrypted = scanner.next();
                  //user.setEncrypted(encrypted);
+
                  //初始
                  user.setVirtualStamps(0);
                  //编号
@@ -115,13 +132,11 @@ public class Meth {
                 //注册日期
                 //user.setRegisterNum(registerNum());
                  System.out.println("Initial virtualstamps:0");
-                 //System.out.println("Your membershipnumber： " + membershipnumber());
+                 System.out.println("Your membershipnumber： " + membershipnumber());
                  //System.out.println("日期是：" + registerNum());
                  //System.out.println(user.toString());  //测试会员信息是否完整
-
                  getMap(user);
             }
-
              //一个判断字符第一位是否是数字的方法
              public static boolean isFirstNum(String string) {
                  Pattern pattern = Pattern.compile(REGEX2);
@@ -142,7 +157,54 @@ public class Meth {
                          return false;
                      }
             }
-            //随机一个编号的方法
+
+    public String strongPW(String string) {
+                //将密码拆分成char数组，遍历这个数组，如果有数字则+1分，有英文小写字母+1分，有英文大写字母+1分，有特殊符号+1分
+                int total = 0;  //给密码强度量化打分
+                int hasNum = 0;   //有数字
+                int hasUpLetter = 0;  //有大写英文字母
+                int hasLowLetter = 0; //有小写英文字母
+                int hasSpecial = 0; //有特殊符号
+                 //设置密码强度的变量名
+                String strong = "";
+                //将传入的密码拆分成char数组
+                char[] c = string.toCharArray();
+                //遍历这个数组
+                for (int i = 0; i < c.length; i++) {
+                         //数字0-9
+                         if ((int) (c[i]) >= 48 && (int) (c[i]) <= 57) {
+                                 hasNum = 1;  //有数字则+1
+                                 //小写英文字母
+                          } else if ((int) (c[i]) >= 97 && (int) (c[i]) <= 122) {
+                               hasLowLetter = 1; //有英文小写字母则+1
+                                //大写英文字母
+                        } else if ((int) (c[i]) >= 65 && (int) (c[i]) <= 90) {
+                            hasUpLetter = 1; //有英文大写字母则+1
+                                //键盘里面的所有特殊符号
+                             } else {
+                                 hasSpecial = 1;
+                             }
+                     }
+                 total = hasLowLetter + hasNum + hasSpecial + hasUpLetter;
+                switch (total) {
+                        case 1:
+                                 strong = "very strong";
+                                 break;
+                        case 2:
+                                 strong = "strong";
+                                 break;
+                         case 3:
+                                 strong = "normal";
+                                 break;
+                        default:
+                                 strong = "not weak";
+                                break;
+                     }
+                 return strong;
+             }
+
+
+            //random menbershipnumber
             public   int membershipnumber() {
                 Random random = new Random();
                 int membershipnumber =random.nextInt(999999 - 100000 + 1) + 100000;
